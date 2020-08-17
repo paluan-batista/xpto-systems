a5aee7a70c30825b7805d765ccedaa1c1b4e52e1
Paluan Batista <paluan.batista@idtrust.com.br>
2020-08-17 15:04:24 -0300

    Adicionando estrutura do projetp

---
+++ src/main/java/br/com/xpto/systems/xptosystems/xptosystems/XptosystemsApplication.java
@@ -0,0 +1,13 @@
+package br.com.xpto.systems.xptosystems.xptosystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XptosystemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XptosystemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/xpto/systems/xptosystems/xptosystems/XptosystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xpto.systems.xptosystems.xptosystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XptosystemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
