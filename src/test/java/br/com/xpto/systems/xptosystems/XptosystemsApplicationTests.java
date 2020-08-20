75b9c287abf18a69537ad13d8fb14da8f760edf4
Paluan Batista <paluan.batista@idtrust.com.br>
2020-08-20 02:46:31 -0300

    Removendo teste gerado com spring initializer

--- src/test/java/br/com/xpto/systems/xptosystems/XptosystemsApplicationTests.java
+++ src/test/java/br/com/xpto/systems/xptosystems/XptosystemsApplicationTests.java
@@ -1,13 +1,8 @@
 package br.com.xpto.systems.xptosystems;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class XptosystemsApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
