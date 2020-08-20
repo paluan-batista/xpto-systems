8e50f0ba4ad75ce06a82810ce5df7d3043237025
Paluan Batista <paluan.batista@idtrust.com.br>
2020-08-20 02:45:11 -0300

    Adicionando classes Jpa

--- src/main/java/br/com/xpto/systems/xptosystems/repository/ArchivesRepository.java
+++ src/main/java/br/com/xpto/systems/xptosystems/repository/ArchivesRepository.java
@@ -4,13 +4,6 @@ import br.com.xpto.systems.xptosystems.model.Cidades;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-public interface CidadesRepository extends JpaRepository<Cidades, Integer> {
-
-    Optional<Cidades> findByIbgeId(int ibgeId);
-
-    List<Cidades> findByCapitalTrue();
+public interface ArchivesRepository extends JpaRepository<Cidades, String> {
 }
